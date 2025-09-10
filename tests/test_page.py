36a2a61fb58be5e7d48eb3ababba3bf6351b5320
artbereh <a.berehovyi@elsevier.com>
2025-09-10 17:42:46 +0200

    commit number 2

--- tests/test_page.py
+++ tests/test_page.py
@@ -1,3 +1,20 @@
 def test_standard_user(login_user):
     start = login_user("standard_user")
     start.check_swag_logo_is_visible()
+
+def test_locked_out_user(login_user):
+    start = login_user("locked_out_user")
+    start.error_message_is_visible()
+    start.error_message_has_correct_text()
+
+def test_problem_user(login_user):
+    start = login_user("problem_user")
+    start.check_swag_logo_is_visible()
+
+def test_performance_glitch_user(login_user):
+    start = login_user("performance_glitch_user")
+    start.check_swag_logo_is_visible()
+
+def test_error_user(login_user):
+    start = login_user("error_user")
+    start.check_swag_logo_is_visible()
